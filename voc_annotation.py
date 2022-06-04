88884f5d295c239360452edc3abb95d2f5b5f682
a-gentili <55603257+a-gentili@users.noreply.github.com>
2022-06-04 19:51:50 +0200

    trying to train on my dataset

--- voc_annotation.py
+++ voc_annotation.py
@@ -1,34 +1,33 @@
 import xml.etree.ElementTree as ET
-from os import getcwd
+import os
 
-sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+sets = ['trainval']
+classes = ['1','2','4']
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-
-
-def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    tree=ET.parse(in_file)
+def convert_annotation(image_id, list_file):
+    in_file = open('/content/dataset/annotations/%s.xml'%(image_id[:-4]))
+    tree= ET.parse(in_file)
     root = tree.getroot()
 
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
+        if cls not in classes:
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
         b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
-wd = getcwd()
+wd = os.getcwd()
 
-for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+for image_set in sets:
+    image_ids = os.listdir('/content/dataset/trainval/')
+    list_file = open('%s.txt'%(image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
-        convert_annotation(year, image_id, list_file)
+        list_file.write('%s/dataset/trainval/%s'%(wd,image_id))
+        convert_annotation(image_id, list_file)
         list_file.write('\n')
     list_file.close()
+
 
