7285c4d9eefd33a60e00d2f0cfeab7983ce0594b
a-gentili <55603257+a-gentili@users.noreply.github.com>
2022-06-05 18:53:38 +0200

    tweaks for colab

--- yolo.py
+++ yolo.py
@@ -20,9 +20,9 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/yolo.h5',
-        "anchors_path": 'model_data/yolo_anchors.txt',
-        "classes_path": 'model_data/coco_classes.txt',
+        "model_path": '/content/keras-yolo3/model_data/yolo.h5',
+        "anchors_path": '/content/keras-yolo3/model_data/yolo_anchors.txt',
+        "classes_path": '/content/keras-yolo3/model_data/voc_classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
@@ -126,7 +126,7 @@ class YOLO(object):
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+        font = ImageFont.truetype(font='/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf',
                     size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = (image.size[0] + image.size[1]) // 300
 
